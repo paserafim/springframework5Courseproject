a7b726eb3d3099083bca9440225f7b7ba615da96
Paulo Serafim <paserafim@gmail.com>
2021-03-20 17:17:50 +0000

    Correct publisher class errors and BootStrapData class errors.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,38 +25,42 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Publisher paulo = new Publisher("Paulo","Rua de Braga","Braga", "Minho", "4700");
+        // save into database
+        publisherRepository.save(paulo);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
-
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+
+        ddd.setPublisher(paulo);
+        paulo.getBooks().add(ddd);
 
         // Save into database
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(paulo);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "39893939339");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(paulo);
+        paulo.getBooks().add(noEJB);
+
         // Save into database
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher paulo = new Publisher("Paulo","Rua de Braga","Braga", "Minho", "4700");
-        // save into database
         publisherRepository.save(paulo);
 
         System.out.println("=========================================");
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Authors: " + authorRepository.count());
-        System.out.println("Number of Authors: " + publisherRepository.count());
-
-
-
-
+        System.out.println("Number of Publishers: " + publisherRepository.count());
+        System.out.println("Publisher Number of Books: " + paulo.getBooks().size());
 
     }
 }
