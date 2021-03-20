763dec0067e352163c1023e926db5d11d7869d1f
Paulo Serafim <paserafim@gmail.com>
2021-03-20 16:52:32 +0000

    Added equals and hashcode methods in Publisher class.

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -79,6 +79,19 @@ public class Publisher {
                 ", zip='" + zip + '\'' +
                 '}';
     }
-    
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
